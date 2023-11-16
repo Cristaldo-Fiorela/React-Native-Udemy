8f3719de2735db23acae3f0fcf064cb31b88e183
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-11-15 21:23:12 -0300

    mostrando usuario de API en pantalla

--- 01-React+Typescript/src/components/Usuarios.tsx
+++ 01-React+Typescript/src/components/Usuarios.tsx
@@ -9,13 +9,32 @@ const Usuarios = () => {
     useEffect(() => {
         // llamado a la API
         reqResApi.get<ReqResListado>('/users')
-            .then (response => {
-                console.log(response.data.data[0].avatar)
+            .then (res => {
+                setUsuarios(res.data.data);
             })
             .catch(err => console.log(err))
     }, [])
-    
 
+    const renderItem = (usuario: Usuario) => {
+        const {id, first_name, last_name, email, avatar} = usuario;
+
+        return (
+            <tr key={id}>
+                <td>
+                    <img 
+                    src={avatar} 
+                    alt={first_name}
+                    style={{
+                        width: 35,
+                        borderRadius: 100
+                    }}
+                    />
+                </td>
+                <td>{first_name} {last_name}</td>
+                <td>{email}</td>
+            </tr>
+        )
+    }
     return (
         <>
             <h3>Usuarios:</h3>
@@ -28,8 +47,17 @@ const Usuarios = () => {
                     </tr>
                 </thead>
                 <tbody>
+                    {
+                        usuarios.map( usuario => renderItem(usuario))
+                    }
                 </tbody>
             </table>
+
+            <button
+                className='btn btn-primary'
+            >
+                Siguiente
+            </button>
         </>
     )
 }
