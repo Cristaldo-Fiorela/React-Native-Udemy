0efbc6cfa187c4a14e331636b57e9c74f272b25e
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-11-21 15:25:19 -0300

    agregando validacion para los campos del formulario

--- 01-React+Typescript/src/hooks/useForm.tsx
+++ 01-React+Typescript/src/hooks/useForm.tsx
@@ -4,7 +4,7 @@ const useForm = <T extends Object>( formulario: T ) => {
     
     const [state, setState] = useState( formulario );
 
-    const onChange = ( value: string, campo: string ) => {
+    const onChange = ( value: string, campo: keyof T ) => {
         setState({
             ...state,
             [campo]: value,
