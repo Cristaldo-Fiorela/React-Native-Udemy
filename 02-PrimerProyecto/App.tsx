08334a390cc7756ad67948fb153c553981ff8321
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-11-27 20:04:02 -0300

    practicando elementos y estilos de native

--- 02-PrimerProyecto/App.tsx
+++ 02-PrimerProyecto/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import { View, Text} from 'react-native';
+import { View, Text } from 'react-native';
 
 const App = () => {
   return (
-    <View>
-      <Text>Hola Mundo</Text>
+    <View style={{
+      flex: 1,
+      justifyContent: 'center',
+    }}>
+      <Text style={{
+        fontSize: 45,
+        textAlign: 'center',
+      }}>
+        Hola Mundo
+      </Text>
     </View>
   )
 }
