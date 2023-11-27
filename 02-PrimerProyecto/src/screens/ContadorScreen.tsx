c2fe2250b644d01445f9ca11a89386a073083037
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-11-27 20:18:25 -0300

    creacion de nueva screen: contador

---
+++ 02-PrimerProyecto/src/screens/ContadorScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const ContadorScreen = () => {
+    return (
+        <View>
+            <Text>Contador: XXX</Text>
+        </View>
+    )
+}
+
+export default ContadorScreen
