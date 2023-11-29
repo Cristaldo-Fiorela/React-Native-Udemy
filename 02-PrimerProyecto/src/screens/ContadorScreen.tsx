a918f68e22f4c56ba6cd0bbb99557e6ec3ac22b8
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-11-29 17:08:48 -0300

    implementando mas de stylesheer al estilo previo

--- 02-PrimerProyecto/src/screens/ContadorScreen.tsx
+++ 02-PrimerProyecto/src/screens/ContadorScreen.tsx
@@ -8,20 +8,14 @@ const ContadorScreen = () => {
 
     return (
         <View style={ styles.container}>
-            <Text style={{
-                textAlign: 'center',
-                fontSize: 40,
-                top: -15
-            }}>Contador: {contador}</Text>
+            <Text style={ styles.title }>
+                Contador: {contador}
+            </Text>
+
             <TouchableOpacity
                 onPress={() => setContador(contador + 1)}
             >
-                <View
-                    style={{ 
-                        backgroundColor: 'red',
-                        borderRadius: 100
-                }}
-                >
+                <View style={styles.btnIncrementar} >
                     <Text>+1</Text>
                 </View>
             </TouchableOpacity>
@@ -35,5 +29,14 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
+    },
+    title: {
+        textAlign: 'center',
+        fontSize: 40,
+        top: -15
+    },
+    btnIncrementar: { 
+        backgroundColor: 'red',
+        borderRadius: 100
     }
 })
