7b3e66a8b49de1865dbed5c190a8b21b2e017431
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-12-04 20:15:20 -0300

    importando a nuesta app el componente BoxObject

--- 02-PrimerProyecto/App.tsx
+++ 02-PrimerProyecto/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 //import HolaMundoScreen from './src/screens/HolaMundoScreen';
-import ContadorScreen from './src/screens/ContadorScreen';
+//import ContadorScreen from './src/screens/ContadorScreen';
+import BoxObjectModelScreen from './src/screens/BoxObjectModelScreen';
 
 const App = () => {
   return (
     //<HolaMundoScreen/>
-    <ContadorScreen />
+    // <ContadorScreen />
+    <BoxObjectModelScreen />
   )
 }
 
