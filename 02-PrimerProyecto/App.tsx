4267ad3177fa248a9169f1d06e8d334ab9f0efae
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-12-05 19:14:44 -0300

    safeAreaView y su uso

--- 02-PrimerProyecto/App.tsx
+++ 02-PrimerProyecto/App.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 //import HolaMundoScreen from './src/screens/HolaMundoScreen';
 //import ContadorScreen from './src/screens/ContadorScreen';
 import BoxObjectModelScreen from './src/screens/BoxObjectModelScreen';
+import { SafeAreaView } from 'react-native';
 
 const App = () => {
   return (
-    //<HolaMundoScreen/>
-    // <ContadorScreen />
-    <BoxObjectModelScreen />
+    <SafeAreaView>
+      {/* <HolaMundoScreen/> */}
+      {/* <ContadorScreen /> */}
+      <BoxObjectModelScreen />
+    </SafeAreaView>
   )
 }
 
