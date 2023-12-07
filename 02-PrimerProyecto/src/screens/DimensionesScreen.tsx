f318a5f4155eba1cb05c019f1fa7a2fe75016823
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-12-07 16:23:39 -0300

    utilizacion de diferentes hooks para adaptar el contenido en base a las dimensiones de la pantalla

--- 02-PrimerProyecto/src/screens/DimensionesScreen.tsx
+++ 02-PrimerProyecto/src/screens/DimensionesScreen.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from 'react';
+import { StyleSheet, Text, View, Dimensions, useWindowDimensions } from 'react-native';
+
+/*  Obteniendo las dimensiones de la pantalla actual con el Hook de react native
+! Al traerlas desde la pantalla esta info no va a cambiar independientemente de si el telefono este en horizontal o vertical.
+* const dimensions = Dimensions.get('window');
+* const { width, height } = dimensions;
+*/
 
 const DimensionesScreen = () => {
+
+    // Hook que expone el ancho y alto de la pantalla en base dependiendo de si el telefono esta en horizontal o vertical
+    const {width, height} = useWindowDimensions();
+
     return (
-        <View style={ styles.container }>
-            <View style={ styles.cajaNaranja }></View>
-            <View style={ styles.cajaMorada }></View>
+        <View>
+            <View style={ styles.container }>
+                <View style={ styles.cajaNaranja }></View>
+                <View style={{ 
+                        ...styles.cajaMorada,
+                        width: width * 0.6
+                    }}></View>
+                <Text style={ styles.title}>W: { width }, H: { height }</Text>
+            </View>
         </View>
     )
 }
@@ -15,7 +31,7 @@ export default DimensionesScreen
 const styles = StyleSheet.create({
     container: {
         width: '100%',
-        height: 600,
+        height: 200,
         backgroundColor: 'red',
     },
     cajaMorada: {
@@ -25,5 +41,9 @@ const styles = StyleSheet.create({
     },
     cajaNaranja: {
         backgroundColor: '#F0A23B'
+    },
+    title: {
+        fontSize: 30,
+        textAlign: 'center',
     }
 });
