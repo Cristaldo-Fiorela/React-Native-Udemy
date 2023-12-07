bc72fa51b8cbcc2843a31d256ab6aad4a0f1dc5a
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-12-07 16:36:48 -0300

    creacion de nueva screen para practicar position

---
+++ 02-PrimerProyecto/src/screens/PositionScreen.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const PositionScreen = () => {
+    return (
+        <View>
+            <Text>Position</Text>
+        </View>
+    )
+}
+
+export default PositionScreen
+
+const styles = StyleSheet.create({
+
+});
