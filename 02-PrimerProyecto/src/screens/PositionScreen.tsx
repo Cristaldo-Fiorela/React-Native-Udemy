1720436ae8b9adbfb6973fb383a4740fd97b4cc4
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-12-07 17:38:39 -0300

    ejercitando position relative

--- 02-PrimerProyecto/src/screens/PositionScreen.tsx
+++ 02-PrimerProyecto/src/screens/PositionScreen.tsx
@@ -3,8 +3,9 @@ import { StyleSheet, Text, View } from 'react-native'
 
 const PositionScreen = () => {
     return (
-        <View>
-            <Text>Position</Text>
+        <View style={ styles.container }>
+            <View style={styles.cajaMorada }/>
+            <View style={styles.cajaNaranja }/>
         </View>
     )
 }
@@ -12,5 +13,29 @@ const PositionScreen = () => {
 export default PositionScreen
 
 const styles = StyleSheet.create({
-
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#28C4D9',
+    },
+    cajaMorada: {
+        width: 100,
+        height: 100,
+        backgroundColor: '#5856D6',
+        borderWidth: 10,
+        borderColor: 'white',
+        // top: -50,
+        // left: 100,
+        right:50
+    },
+    cajaNaranja: {
+        width: 100,
+        height: 100,
+        backgroundColor: '#F0A23B',
+        borderWidth: 10,
+        borderColor: 'white',
+        top: -50,
+        left: 50
+    }
 });
