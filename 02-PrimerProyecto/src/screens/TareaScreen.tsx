08dc0db2f6475fb3257b72dde57f5b8dd14d56a9
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-12-14 18:04:09 -0300

    resolucion de ejercicios 1 a 10 utilizando flex, align, justify y position

--- 02-PrimerProyecto/src/screens/TareaScreen.tsx
+++ 02-PrimerProyecto/src/screens/TareaScreen.tsx
@@ -17,6 +17,17 @@ const styles = StyleSheet.create({
     container: {
         backgroundColor: '#28425B',
         flex: 1,
+        // * EJERCICIO 2
+        // justifyContent: 'center',
+        // * EJERCICIO 4
+        // justifyContent: 'space-between',
+        // * EJERCICIO 5
+        // flexDirection: 'row',
+        // * EJERCICIO 6 
+        justifyContent: 'center',
+        alignItems: 'center',
+        // *EJERCICIO 10
+        flexDirection: 'row'
     },
     cajaMorada: {
         width: 100,
@@ -24,6 +35,18 @@ const styles = StyleSheet.create({
         borderWidth: 10,
         borderColor: 'white',
         backgroundColor: '#5856D6',
+        // * EJERCICIO 3
+        // alignSelf: 'flex-end',
+        // * EJERCICIO 4
+        // alignSelf: 'flex-end'
+        // * EJERCICIO 5
+        // // height: 100,
+        // * EJERCICIO 6
+        // flex: 2,
+        // // width: 100,
+        // * EJERCICIO 8
+        // top: 100
+
     },
     cajaNaranja: {
         width: 100,
@@ -31,6 +54,19 @@ const styles = StyleSheet.create({
         borderWidth: 10,
         borderColor: 'white',
         backgroundColor: '#F0A23B',
+        //* EJERCICIO 1
+        // flex: 1
+        // * EJERCICIO 4
+        // alignSelf: 'center',
+        // * EJERCICIO 5
+        // // height: 100,
+        // * EJERCICIO 6
+        // flex: 2
+        // // width: 100,
+        // * EJERCICIO 7
+        // left: 100
+        // * EJERCICIO 10
+        bottom: -50
     },
     cajaAzul: {
         width: 100,
@@ -38,5 +74,14 @@ const styles = StyleSheet.create({
         borderWidth: 10,
         borderColor: 'white',
         backgroundColor: '#28C4D9',
+        // * EJERCICIO 2
+        // // width: 100,
+        // * EJERCICIO 3
+        // alignSelf: 'center',
+        // * EJERCICIO 5
+        // // height: 100,    
+        // * EJERCICIO 6
+        // flex: 4,
+        // // width: 100,
     },
 });
