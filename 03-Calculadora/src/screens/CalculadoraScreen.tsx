042ba30adb7c5acccdfdee2fb2822048aecda3c4
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-12-26 16:39:21 -0300

    creacion de primer boton y estilos del mismo

--- 03-Calculadora/src/screens/CalculadoraScreen.tsx
+++ 03-Calculadora/src/screens/CalculadoraScreen.tsx
@@ -4,8 +4,17 @@ import styles from '../theme/appTheme';
 
 const CalculadoraScreen = () => {
     return (
-        <View>
-            <Text style={ styles.texto }>Calculadora Screen</Text>
+        <View style={ styles.calculadoraContainer }>
+            <Text style={ styles.resultadoPequeno }>1,500.00</Text>
+            <Text style={ styles.resultado }>1,500.00</Text>
+
+
+            <View>
+                {/* Boton */}
+                <View style={ styles.boton }>
+                    <Text style={ styles.botonTexto }>C</Text>
+                </View>
+            </View>
         </View>
     )
 }
--- 03-Calculadora/src/theme/appTheme.tsx
+++ 03-Calculadora/src/theme/appTheme.tsx
@@ -5,9 +5,34 @@ const styles = StyleSheet.create({
         backgroundColor: 'black',
         flex: 1,
     },
-    texto: {
+    calculadoraContainer: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        paddingHorizontal: 10,
+    },
+    resultado: {
         color: 'white',
-        fontSize: 20, 
+        fontSize: 60, 
+        textAlign: 'right',
+    },
+    resultadoPequeno: {
+        color: 'rgba(255, 255, 255, 0.5)',
+        fontSize: 30, 
+        textAlign: 'right',
+    },
+    boton: {
+        backgroundColor: '#9B9B9B',
+        borderRadius: 100,
+        height: 80,
+        justifyContent: 'center',
+        width: 80,
+    },
+    botonTexto: {
+        color: 'black',
+        fontSize: 30,
+        fontWeight: 'bold',
+        padding: 10,
+        textAlign: 'center',
     }
 });
 
